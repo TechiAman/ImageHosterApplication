bb1cc9e4cf2cd896490e6f34b9e21844e19eb078
Abhishek Aman <abhishek.aman@trakinvest.com>
2019-09-23 21:44:40 +0530

    fixed issue of not allowing non owners of image to delete the image

--- src/main/java/ImageHoster/controller/ImageController.java
+++ src/main/java/ImageHoster/controller/ImageController.java
@@ -156,9 +156,24 @@ public class ImageController {
     //The method calls the deleteImage() method in the business logic passing the id of the image to be deleted
     //Looks for a controller method with request mapping of type '/images'
     @RequestMapping(value = "/deleteImage", method = RequestMethod.DELETE)
-    public String deleteImageSubmit(@RequestParam(name = "imageId") Integer imageId) {
-        imageService.deleteImage(imageId);
-        return "redirect:/images";
+    public String deleteImageSubmit(@RequestParam(name = "imageId") Integer imageId, HttpSession session, Model model) {
+
+        User user = (User) session.getAttribute("loggeduser");
+        Image image = imageService.getImage(imageId);
+
+        //check whether the user trying to edit is the owner of the image or not
+        if(this.imageService.confirmOwner(imageId, user.getUsername())){
+            //yes delete the image
+            this.imageService.deleteImage(imageId);
+            return "redirect:/images";
+        }else{
+            String error = "Only the owner of the image can delete the image";
+            model.addAttribute("deleteError", error);
+            model.addAttribute("image", image);
+            return "images/image";
+        }
+
+
     }
 
 
